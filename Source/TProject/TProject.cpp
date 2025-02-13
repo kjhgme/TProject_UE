f0773f733980dc9a3b198998fc64a99cf6651d4d
kjhgme <kjhgme@naver.com>
2025-02-13 15:18:29 +0900

    프로젝트 생성.

---
+++ Source/TProject/TProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TProject, "TProject" );
